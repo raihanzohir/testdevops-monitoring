26cf75e022dd46d697453e51a27c05de74d604d0
raihanzohir <raihanzohir@gmail.com>
2022-01-16 18:12:03 +0600

    Country Entity Updated

--- src/main/java/com/simplilearn/devops/testdevops/monitoring/models/Country.java
+++ src/main/java/com/simplilearn/devops/testdevops/monitoring/models/Country.java
@@ -7,14 +7,9 @@ import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.util.Objects;
-
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -24,50 +19,49 @@ public class Country {
     @Id
     @GeneratedValue
     private int id;
-
     private String name;
     
-    public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-		
-
-	public Country(String name) {
-		this.name = name;
-	}
-
-	@Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        Country country = (Country) o;
-        return id == country.id && name.equals(country.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-    @Override
-    public String toString() {
-        return "Country{" +
-          "id=" + id +
-          ", name='" + name + '\'' +
-          '}';
-    }
+//    public int getId() {
+//		return id;
+//	}
+//
+//	public void setId(int id) {
+//		this.id = id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//		
+//
+//	public Country(String name) {
+//		this.name = name;
+//	}
+//
+//	@Override
+//    public boolean equals(Object o) {
+//        if (this == o)
+//            return true;
+//        if (o == null || getClass() != o.getClass())
+//            return false;
+//        Country country = (Country) o;
+//        return id == country.id && name.equals(country.name);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id, name);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Country{" +
+//          "id=" + id +
+//          ", name='" + name + '\'' +
+//          '}';
+//    }
 }
